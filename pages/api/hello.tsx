2870f3f920e7b9c176e4b1b5e61dc688af0faec1
tianyou-song <tim.d.song@gmail.com>
2020-11-30 02:47:15 -0800

    example fix
    Signed-off-by: tianyou-song <tim.d.song@gmail.com>

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -5,7 +5,15 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const api = (req: NextApiRequest, res: NextApiResponse): void => {
 	// eslint-disable-next-line no-magic-numbers
 	res.statusCode = 200;
-	res.json({ name : "John Doe" });
+	res.json({
+		body        : JSON.stringify(req.body),
+		cookies     : req.cookies,
+		env         : req.env,
+		name        : "John Doe",
+		preview     : req.preview,
+		previewData : JSON.stringify(req.previewData),
+		query       : req.query,
+	});
 };
 
 export default api;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import type { NextPage } from "next";
 import React from "react";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
 	return (
